22f64fe48ce2128445e5dc098dc566a2655de1f3
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-07-02 23:25:22 -0500

    Modify firestore model so now works as asyn/await

--- app/models/firestore-model.js
+++ app/models/firestore-model.js
@@ -2,83 +2,81 @@ const FirestoreModel = function (firestore) {
     this.getAll = function (table) {
         return new Promise((resolve, reject) => {
             firestore.collection(table).get()
-            .then((registros)=>{
-                let respuesta =[];
-                registros.forEach((registro) => {
-                    let elemento = registro.data();
-                    elemento.id = registro.id;
-                    respuesta.push(elemento);
-                });
+                .then((registros) => {
+                    let respuesta = [];
+                    registros.forEach((registro) => {
+                        let elemento = registro.data();
+                        elemento.id = registro.id;
+                        respuesta.push(elemento);
+                    });
 
-                resolve(respuesta);
-            })
-            .catch((error)=>{
-                reject(error);
-            });
+                    resolve(respuesta);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
     };
 
     this.getById = function (table, id) {
         return new Promise((resolve, reject) => {
             firestore.collection(table).doc(id).get()
-            .then((registro)=>{
-                if (registro.exists){
-                    let respuesta = registro.data();
-                    respuesta.id = registro.id;
-                    resolve(respuesta);
-                }else{
-                    reject({error: 'El elemento en la colección ' + table+' no existe'});
-                }                
-            })
-            .catch((error)=>{
-                reject(error);
-            });
+                .then((registro) => {
+                    if (registro.exists) {
+                        let respuesta = registro.data();
+                        respuesta.id = registro.id;
+                        resolve(respuesta);
+                    } else {
+                        reject({ error: 'El elemento en la colección ' + table + ' no existe' });
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
     };
 
-    this.create = function (table, params) {
-        return new Promise((resolve, reject) => {
-            firestore.collection(table).add(params)
-            .then((respuesta)=>{
-                params.id = respuesta.id;                
-                resolve(params);
-            })
-            .catch((error)=>{
-                reject(error);
-            });
-        });
+    this.create = async (table, params) => {
+        try {
+            const response = await firestore.collection(table).add(params)
+            params.id = response.id
+        }
+        catch {
+            error => console.log(error)
+        }
+
     };
 
     this.update = function (table, params, id) {
         return new Promise((resolve, reject) => {
             firestore.collection(table).doc(id).update(params)
-            .then((respuesta)=>{        
-                params.id = respuesta.id;
-                resolve(params);                
-            })
-            .catch((error)=>{
-                reject(error);
-            });
+                .then((respuesta) => {
+                    params.id = respuesta.id;
+                    resolve(params);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
     };
 
     this.delete = function (table, id) {
         return new Promise((resolve, reject) => {
             firestore.collection(table).doc(id).delete()
-            .then((respuesta)=>{                
-                resolve('Elemento eliminado' + id);
-            })
-            .catch((error)=>{
-                reject(error);
-            });
+                .then((respuesta) => {
+                    resolve('Elemento eliminado' + id);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
     };
-    
+
     this.clean = function (table) {
         return new Promise((resolve, reject) => {
         });
     };
-    
+
     this.initialize = function (table, params) {
         return new Promise((resolve, reject) => {
             resolve('No aplica para Firestore');
