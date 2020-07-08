9c5e28107f2d63f2a6de942f157ed63f68ce7900
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-07-08 00:16:38 -0500

    Add users interface and deployed to firebase

--- pet-lovers/src/app/tab2/tab2.page.ts
+++ pet-lovers/src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, ToastController } from '@ionic/angular';
+
+import { Users } from '../model/users';
 
 @Component({
   selector: 'app-tab2',
@@ -8,7 +10,41 @@ import { ActionSheetController } from '@ionic/angular';
 })
 export class Tab2Page {
 
-  constructor(private actionSheetController: ActionSheetController) {}
+  constructor(
+    private actionSheetController: ActionSheetController,
+    private toastController: ToastController) {}
+
+  users: Users[] = [];
+
+  ngOnInit() {
+    this.users.push({ 
+      title: 'A good loking header', 
+      subtitle: 'handsome subtitle',
+      avatar: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fthefilmstage.com%2Fwp-content%2Fuploads%2F2012%2F04%2Fneytiri_beautiful_warrior_in_avatar-wide.jpg&f=1&nofb=1',
+      description: 'Avatar from avatar'},
+      {
+        title: 'Another heading',
+        subtitle: 'handsome subtitle',
+        avatar: 'http://fc04.deviantart.net/fs71/i/2010/182/c/0/Facebook_Avatar_2_by_cynfullpryde.jpg',
+        description: 'A good looking avatar'
+      })
+  }
+
+  async userSelected(user) {
+    const toast = await this.toastController.create({
+      header: `You've chosen user ${user.title}`,
+      position: 'bottom',
+      message: `I'm ${user.description}`,
+      buttons: [{
+        handler: () => {
+          this.users.pop()
+        },
+        text: 'Destroy',
+        icon: 'trash'
+      }]
+    });
+    toast.present();
+  }
 
   async showSheet() {
     const actionSheet = await this.actionSheetController.create({
